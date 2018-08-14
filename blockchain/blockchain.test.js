c9ae91c621c81b71e3a0fa1c8c75acadaa87ca30
Dman89 <Daniel@DanielCudney.com>
2018-08-14 12:10:30 -0700

    add chain validation and corresponding tests

--- blockchain/blockchain.test.js
+++ blockchain/blockchain.test.js
@@ -2,9 +2,10 @@ const Blockchain = require('./index');
 const Block = require('../block');
 
 describe('Blockchain', () => {
-  let blockchain;
+  let blockchain, blockchain2;
   beforeEach(() => {
     blockchain = new Blockchain();
+    blockchain2 = new Blockchain();
   });
 
   it('starts with the genesis block', () => {
@@ -12,8 +13,24 @@ describe('Blockchain', () => {
   });
 
   it('adds a new block', () => {
-    const data = 'foo';
+    const data = 'Valid Input';
     blockchain.addBlock(data);
     expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(data);
+  });
+
+  it('validates a valid chain', () => {
+  	blockchain2.addBlock('Valid Input');
+  	expect(blockchain.isValidChain(blockchain2.chain)).toBe(true);
+  });
+
+  it('invalidates a chain with a corrupt genesis block', () => {
+  	blockchain2.chain[0].data = 'Corrupt Input';
+    expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
+  });
+
+  it('invalidates a corrupt chain', () => {
+    blockchain2.addBlock('Valid Input');
+    blockchain2.chain[1].data = 'Corrupt Input';
+    expect(blockchain.isValidChain(blockchain2.chain)).toBe(false);
   });
 });
