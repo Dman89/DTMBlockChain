b5e0afd8d8049f0c0413cf318bdf4881776024f2
Dman89 <Daniel@DanielCudney.com>
2018-08-14 12:50:32 -0700

    Add a post endpoint to the application and required npm packages
    Package:
    body-parser

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
@@ -6,8 +7,15 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const blockchain = new Blockchain();
 
+app.use(bodyParser.json());
 app.get('/blocks', (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post('/mine', (req, res) => {
+  const block = blockchain.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+  res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on the port ${HTTP_PORT}`));
