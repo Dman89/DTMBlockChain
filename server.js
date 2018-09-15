75282ba6266683a7371e0134af204390508d44a5
Dman89 <Daniel@DanielCudney.com>
2018-09-15 12:35:40 -0700

    Removed commented out code in the server file
    What:
    
    Why:
    
    Tags:

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ const p2pServer = new P2pServer(blockchain, tp);
 const miner = new Miner(blockchain, tp, wallet, p2pServer);
 
 app.use(bodyParser.json());
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 
 app.use("/", express.static(path.join(__dirname, 'public')))
 
